20689d3462e0aa51b2c242d90135854468a3040d
bekri <you@example.com>
2021-11-21 00:09:22 +0300

    maymun2

--- test.go
+++ test.go
@@ -8,6 +8,7 @@ import ("github.com/gin-gonic/gin"
 type Sex struct {
 	Date time.Time `json:"date"`
 	Coin string `json:"coin"`
+	ResolutionNo uint `json:"resolutionNo"`
 	Resolution string `json:"resolution"`
 	Signal string `json:"signal"`
 	Price string `json:"price"`
